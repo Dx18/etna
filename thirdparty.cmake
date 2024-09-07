14ec74b7d7d2d738c2458ffe1f7ca582a0f74682
Dx18 <Dx18.E@yandex.ru>
2024-09-07 11:16:30 +0300

    Fixed SPIRV-Reflect git tag selection

--- thirdparty.cmake
+++ thirdparty.cmake
@@ -1,6 +1,66 @@
 cmake_minimum_required(VERSION 3.25)
 
 
+function (find_suitable_git_version_tag
+    REPO_URL TAG_PREFIX MIN_VERSION
+    OUT_STATUS_VARIABLE OUT_TAG_VARIABLE
+  )
+
+  find_package(Git)
+  if (NOT Git_FOUND)
+    set(${OUT_STATUS_VARIABLE} FALSE PARENT_SCOPE)
+    set(${OUT_TAG_VARIABLE} "" PARENT_SCOPE)
+    return()
+  endif()
+
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} ls-remote --tags ${REPO_URL}
+    RESULT_VARIABLE TAGS_RESULT
+    OUTPUT_VARIABLE TAG_LIST
+  )
+
+  if (${TAGS_RESULT} AND NOT ${TAGS_RESULT} EQUAL 0)
+    set(${OUT_STATUS_VARIABLE} FALSE PARENT_SCOPE)
+    set(${OUT_TAG_VARIABLE} "" PARENT_SCOPE)
+    return()
+  endif()
+
+  string(REGEX REPLACE
+    "[a-f0-9]+\trefs/tags/${TAG_PREFIX}([^\r\n]+)\r?\n" "\\1\n"
+    VERSION_LIST "${TAG_LIST}")
+  string(REGEX REPLACE
+    "[a-f0-9]+\trefs/tags/([^\r\n]+)\r?\n" ""
+    VERSION_LIST "${VERSION_LIST}")
+  string(REPLACE "\n" ";"
+    VERSION_LIST "${VERSION_LIST}")
+
+
+  set(BEST_VERSION "")
+  foreach (VERSION IN LISTS VERSION_LIST)
+    if (
+	(NOT "${VERSION}" STREQUAL "") AND
+	("${VERSION}" VERSION_GREATER_EQUAL "${MIN_VERSION}")
+      )
+      if (
+	  ("${BEST_VERSION}" STREQUAL "") OR
+	  ("${VERSION}" VERSION_LESS "${BEST_VERSION}")
+	)
+	set(BEST_VERSION "${VERSION}")
+      endif()
+    endif()
+  endforeach()
+
+  if ("${BEST_VERSION}" STREQUAL "")
+    set(${OUT_STATUS_VARIABLE} FALSE PARENT_SCOPE)
+    set(${OUT_TAG_VARIABLE} "" PARENT_SCOPE)
+    return()
+  endif()
+
+  set(${OUT_STATUS_VARIABLE} TRUE PARENT_SCOPE)
+  set(${OUT_TAG_VARIABLE} "${TAG_PREFIX}${BEST_VERSION}" PARENT_SCOPE)
+endfunction()
+
+
 find_package(Vulkan 1.3.275 REQUIRED)
 
 # GPU-side allocator for Vulkan by AMD
@@ -29,10 +89,34 @@ endif ()
 # Official library for parsing SPIRV bytecode
 # We grab the exact SDK version from github so that
 # you don't have to remember to mark a checkbox while installing the SDK =)
+#
+# UPD 2024.09.07: SPIRV-Reflect repository does not have tags for all
+# the Vulkan versions. For example, there are tags
+# `vulkan-sdk-1.3.283.0` and `vulkan-sdk-1.3.290.0`, but no tags
+# "covering" versions between "1.3.283.0" and
+# "1.3.290.0". Furthermore, the version of Vulkan may contain only
+# three components (major, minor and patch), while in tags there are
+# four-component versions (major, minor, patch and tweak). Therefore,
+# the tag selection has to be more elaborate
+
+find_suitable_git_version_tag(
+  https://github.com/KhronosGroup/SPIRV-Reflect
+  vulkan-sdk- ${Vulkan_VERSION}
+  SPIRV_REFLECT_GIT_TAG_STATUS
+  SPIRV_REFLECT_GIT_TAG
+)
+
+if (NOT ${SPIRV_REFLECT_GIT_TAG_STATUS})
+  message(
+    WARNING
+    "Could not find a suitable git tag for SPIRV-Reflect. Using the exact Vulkan version")
+  set(SPIRV_REFLECT_GIT_TAG ${Vulkan_VERSION})
+endif()
+
 CPMAddPackage(
   NAME SpirvReflect
   GITHUB_REPOSITORY KhronosGroup/SPIRV-Reflect
-  GIT_TAG "vulkan-sdk-${Vulkan_VERSION}"
+  GIT_TAG "${SPIRV_REFLECT_GIT_TAG}"
   OPTIONS
     "SPIRV_REFLECT_EXECUTABLE OFF"
     "SPIRV_REFLECT_STRIPPER OFF"
